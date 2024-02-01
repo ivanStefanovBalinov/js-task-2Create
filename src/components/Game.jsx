5fde43f3a38755fa407a729c03eabed28dd1fb55
ivanStefanovBalinov <116738791+ivanStefanovBalinov@users.noreply.github.com>
2024-02-01 20:49:57 +0200

    Update Layout Style

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import GameBoard from "./GameBoard";
+import "../Styles/Game.css";
 
 export default function () {
   return (
-    <>
+    <div className="game">
       <GameBoard />
-    </>
+    </div>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,10 +4,19 @@ import "../Styles/GameBoard.css";
 
 export default function GameBoard() {
   const [cells, setsCell] = useState(Array(9).fill(null));
+
+  const handleClickCell = (id) => {
+    console.log(id);
+  };
+
   return (
     <div className="game-board">
       {cells.map((cell, index) => (
-        <Cell key={index + 1} />
+        <Cell
+          key={index + 1}
+          id={index + 1}
+          onClick={() => handleClickCell(index + 1)}
+        />
       ))}
     </div>
   );
